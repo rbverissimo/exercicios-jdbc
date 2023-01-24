0cf165939146dc2edc58537b7c364bb13416b299
rbverissimo <rverissimo.dev@gmail.com>
2023-01-23 23:03:37 -0300

    Criação da classe criarTabelaPessoas que usa a conexão com as informações de conexão passadas para FabricaConexao

--- src/jdbc/FabricaConexao.java
+++ src/jdbc/FabricaConexao.java
@@ -1,5 +1,24 @@
 package jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class FabricaConexao {
+	
+	public static Connection getConexao() {
+		
+		try {
+			String url = "jdbc:mysql://localhost/curso_java"
+					+ "?verifyServerCertificate=false&useSSL=true";
+			String usuario = "root";
+			String senha = "123456";
+			
+			return DriverManager.getConnection(url, 
+					usuario, senha);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 }
