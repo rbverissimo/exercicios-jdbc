9d4996e8ffb9b235fb084fa2e352a048ff0db2b8
rbverissimo <rverissimo.dev@gmail.com>
2023-01-23 22:39:19 -0300

    Estabelecendo conexão usando a Connection do java.sql

---
+++ src/jdbc/TesteConexao.java
@@ -0,0 +1,24 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteConexao {
+	
+	public static void main(String[] args) throws SQLException {
+		
+		String url = "jdbc:mysql://localhost"
+				+ "?verifyServerCertificate=false&useSSL=true";
+		String usuario = "root";
+		String senha = "123456";
+		
+		Connection conexao = DriverManager.getConnection(url, 
+				usuario, senha);
+		
+		System.out.println("Conexão efetuada com sucesso!");
+		conexao.close();
+		
+	}
+
+}
