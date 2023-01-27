8b6e9d1758e99d4828eac3334f3dde4068a98e3b
rbverissimo <rverissimo.dev@gmail.com>
2023-01-27 10:55:38 -0300

    Construção de um código com PreparedStatement que torna o banco de dados seguro em relação ao SQLInjection

--- src/jdbc/NovaPessoa.java
+++ src/jdbc/NovaPessoa.java
@@ -1,17 +1,27 @@
 package jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class NovaPessoa {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		
 		Scanner entrada = new Scanner(System.in);
 		
 		System.out.println("Informe o nome: ");
 		String nome = entrada.nextLine();
 		
-		String sql = "INSERT INTO pessoas(nome) values ('" + nome + "')";
+		Connection conexao = FabricaConexao.getConexao();
 		
+		String sql = "INSERT INTO pessoas(nome) values (?)";
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		stmt.setString(1, nome);
+		stmt.execute();
+		
+		System.out.println("Pessoa incluída com sucesso!");
 		entrada.close();
 	}
 
