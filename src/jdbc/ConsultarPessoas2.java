3fe1f687b89029abb50820557243daaf526efb03
rbverissimo <rverissimo.dev@gmail.com>
2023-01-27 11:54:55 -0300

    Criando um script de consulta usando like na claúsula where para encontrar somente determinados valores dentro das Strings do banco de dados

---
+++ src/jdbc/ConsultarPessoas2.java
@@ -0,0 +1,46 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ConsultarPessoas2 {
+	
+	public static void main(String[] args) throws SQLException{
+		
+		Connection conexao = FabricaConexao.getConexao();
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Informe as letras: ");
+		String letras = entrada.nextLine();
+		
+		
+		
+		String sql = "select * from pessoas where nome like ?";
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		stmt.setString(1, "%" + letras + "%");
+		ResultSet resultado = stmt.executeQuery();
+		
+		List<Pessoa> pessoas = new ArrayList<>();
+		
+		while(resultado.next()) {
+			int codigo = resultado.getInt("codigo");
+			String nome = resultado.getString("nome");
+			pessoas.add(new Pessoa(codigo, nome));
+		}
+		
+		for(Pessoa p: pessoas) {
+			System.out.println(p.getCodigo() + " ==> " 
+							+ p.getNome());
+		}
+		
+		entrada.close();
+		conexao.close();
+		
+	}
+
+}
