5eda7435c0246d12c3d6e8fbee7d39ccb44dc1f7
rbverissimo <rverissimo.dev@gmail.com>
2023-01-27 11:20:32 -0300

    Criada classe de ConsultarPessoas1 fazendo o mapeamento do ResultSet obtido pelo JDBC para um ArrayList da classe Pessoa. Sendo assim, eu consigo um mapeamento de objetos extraídos do banco de dados para o paradigma orientado a objetos.

--- src/jdbc/Pessoa.java
+++ src/jdbc/Pessoa.java
@@ -4,6 +4,13 @@ public class Pessoa {
 	private int codigo;
 	private String nome;
 	
+	
+	
+	public Pessoa(int codigo, String nome) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+	}
 	public int getCodigo() {
 		return codigo;
 	}
