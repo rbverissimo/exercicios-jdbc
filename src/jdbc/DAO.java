dabbb494bd6b1ca66aa0326eabc2a069549a502e
rbverissimo <rverissimo.dev@gmail.com>
2023-01-28 13:52:59 -0300

    Implementação completa de um método DAO que recebe uma String e um Object genérico com atributos e usa os métodos de getConexao e adicionarAtributos para fazer a abstração dos tipos que serão recebidos. Dessa forma, apenas m método consegue, dentro da classe DAO, fazer a inserção. O mesmo poderia acontecer com qualquer classe DAO para qualquer operação do CRUD.

--- src/jdbc/DAO.java
+++ src/jdbc/DAO.java
@@ -1,11 +1,57 @@
 package jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DAO {
 	
 	private Connection conexao;
+	
+	
+	public int incluir(String sql, Object... atributos) {
+		try {
+			PreparedStatement stmt = getConexao()
+					.prepareStatement(sql, 
+							PreparedStatement.RETURN_GENERATED_KEYS);
+			adicionarAtributos(stmt, atributos);
+			
+			if(stmt.executeUpdate() > 0) {
+				ResultSet r = stmt.getGeneratedKeys();
+				if(r.next()) {
+					return r.getInt(1);
+				}
+			}
+			
+			
+			return -1;
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+	private void adicionarAtributos(PreparedStatement stmt,
+			Object[] atributos) throws SQLException {
+		
+		int indice = 1; 
+		for(Object atributo: atributos) {
+			
+			if(atributo instanceof String) {
+			stmt.setString(indice, (String) atributo);
+			} else if (atributo instanceof Integer){
+				stmt.setInt(indice, (Integer) atributo);	
+			} else if(atributo instanceof Double) {
+				stmt.setDouble(indice, (Double) atributo);
+			}
+			
+			indice++;
+		}
+		
+	}
+	
+	
 	private Connection getConexao() {
 		try {
 			if(conexao != null && !conexao.isClosed()) {
